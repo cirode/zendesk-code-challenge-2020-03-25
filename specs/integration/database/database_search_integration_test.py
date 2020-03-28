185d9c13aa38c394b615422b4e32f82f6a3736df
Chris Rode <cirode@gmail.com>
2020-03-28 17:38:30 +1100

    Moved the database.from_file_path test to integrations
    technically its an integration test

--- specs/integration/database/database_search_integration_test.py
+++ specs/integration/database/database_search_integration_test.py
@@ -94,3 +94,38 @@ class TestDatabaseSearchIntegration__multiple_tables():
 		result = self.database.search("users","_id", expected_result["_id"], include_links=True)[0]
 		result_org = [value for attr,value in result.items() if re.match('^organisation$',attr)]
 		expect(result_org).to(equal([self.org]))
+
+
+from zearch.database import Database, InvalidSchemaException, IndexedTable, Link, ReverseLink
+##
+# Technically an integration test as not mocking. The mocking was becoming pretty arduous, 
+# This should tell me something about the modelling
+# TODO: Look at this
+class TestDatabase__from_file_dir():
+
+	def setup_method(self):
+		self.file_dir = Path('.','specs','sample_files')
+
+	## Schema Testing
+	def test_when_given_a_dir_that_does_not_contain_the_files_listed_by_schema_raises_invalid_schema_exception(self):
+		schema = {"woops":{"primary_key":"_id"}}
+		expect(lambda : Database.from_file_dir(self.file_dir, schema)).to(raise_error(InvalidSchemaException))
+
+
+	def test_when_given_a_dir_that_does_contain_the_files_listed_by_schema_returns_database_with_tables(self):
+		schema = {"tickets":{"primary_key":"_id"}, "users":{"primary_key":"_id"}}
+		database = Database.from_file_dir(self.file_dir, schema)
+		expect([t.name for t in database.tables]).to(equal(["tickets","users"]))
+
+	### With foreign key info in schema
+	def test_when_table_has_foreign_key_adds_reverse_link_to_other_table(self):
+		schema = {"tickets":{"primary_key":"_id","foreign_keys":{"submitter_id": {"name": "submitter", "reverse_name": "submitted_ticket", "table": "users"}}}, "users":{"primary_key":"_id"}}
+		database = Database.from_file_dir(self.file_dir, schema)
+		users_table = [t for t in database.tables if t.name == 'users'][0]
+		expect(users_table.linked_from()).to(equal([ReverseLink(key="submitter_id", name="submitted_ticket",table="tickets")]))
+
+	def test_when_table_has_foreign_key_in_reverse_order_adds_reverse_link_to_other_table(self):
+		schema = {"users":{"primary_key":"_id"},"tickets":{"primary_key":"_id","foreign_keys":{"submitter_id": {"name": "submitter", "reverse_name": "submitted_ticket", "table": "users"}}}}
+		database = Database.from_file_dir(self.file_dir, schema)
+		users_table = [t for t in database.tables if t.name == 'users'][0]
+		expect(users_table.linked_from()).to(equal([ReverseLink(key="submitter_id", name="submitted_ticket",table="tickets")]))
--- specs/unit/database/database_test.py
+++ specs/unit/database/database_test.py
@@ -4,39 +4,6 @@ from pathlib import Path
 import tempfile
 
 
-##
-# Technically an integration test as not mocking. The mocking was becoming pretty arduous, 
-# This should tell me something about the modelling
-# TODO: Look at this
-class TestDatabase__from_file_dir():
-
-	def setup_method(self):
-		self.file_dir = Path('.','specs','sample_files')
-
-	## Schema Testing
-	def test_when_given_a_dir_that_does_not_contain_the_files_listed_by_schema_raises_invalid_schema_exception(self):
-		schema = {"woops":{"primary_key":"_id"}}
-		expect(lambda : Database.from_file_dir(self.file_dir, schema)).to(raise_error(InvalidSchemaException))
-
-
-	def test_when_given_a_dir_that_does_contain_the_files_listed_by_schema_returns_database_with_tables(self):
-		schema = {"tickets":{"primary_key":"_id"}, "users":{"primary_key":"_id"}}
-		database = Database.from_file_dir(self.file_dir, schema)
-		expect([t.name for t in database.tables]).to(equal(["tickets","users"]))
-
-	### With foreign key info in schema
-	def test_when_table_has_foreign_key_adds_reverse_link_to_other_table(self):
-		schema = {"tickets":{"primary_key":"_id","foreign_keys":{"submitter_id": {"name": "submitter", "reverse_name": "submitted_ticket", "table": "users"}}}, "users":{"primary_key":"_id"}}
-		database = Database.from_file_dir(self.file_dir, schema)
-		users_table = [t for t in database.tables if t.name == 'users'][0]
-		expect(users_table.linked_from()).to(equal([ReverseLink(key="submitter_id", name="submitted_ticket",table="tickets")]))
-
-	def test_when_table_has_foreign_key_in_reverse_order_adds_reverse_link_to_other_table(self):
-		schema = {"users":{"primary_key":"_id"},"tickets":{"primary_key":"_id","foreign_keys":{"submitter_id": {"name": "submitter", "reverse_name": "submitted_ticket", "table": "users"}}}}
-		database = Database.from_file_dir(self.file_dir, schema)
-		users_table = [t for t in database.tables if t.name == 'users'][0]
-		expect(users_table.linked_from()).to(equal([ReverseLink(key="submitter_id", name="submitted_ticket",table="tickets")]))
-
 class TestDatabase__fields_for_table():
 	def setup_method(self):
 		self.table = MagicMock(spec=IndexedTable)
